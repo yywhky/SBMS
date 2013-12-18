0472e6f95d8a997ebdb6129aefd1b231de2faa2e
rww <weiweirong1990@gmail.com>
2013-12-18 12:01:51 +0800

    修改登陆页面，增加页面模板template.xhtml，大家可以参考

---
+++ src/sbms/bean/User.java
@@ -0,0 +1,30 @@
+package sbms.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class User {
+	
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void login(){
+		System.out.print("User login1234!");
+	}
+	
+	
+
+}
